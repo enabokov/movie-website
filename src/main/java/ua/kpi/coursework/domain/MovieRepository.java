f41a2cb426ed9452487c6f04491871490d27034d
enabokov <edwardnabokov@gmail.com>
2018-05-14 21:41:45 +0300

    Add queries with pagination by genre in repositories and service.

--- src/main/java/ua/kpi/coursework/domain/MovieRepository.java
+++ src/main/java/ua/kpi/coursework/domain/MovieRepository.java
@@ -1,11 +1,21 @@
 package ua.kpi.coursework.domain;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface MovieRepository extends CrudRepository<Movie, Integer> {
 
-    List<Movie> findByYear(Integer year);
+    List<Movie> findMoviesByYear(Integer year, Pageable pageable);
 
+    Integer countMoviesByYear(Integer year);
+
+    @Query("SELECT DISTINCT m.genre FROM Movie m")
+    List<String> findDistinctGenre();
+
+    List<Movie> findMoviesByGenre(String genre, Pageable pageable);
+
+    Integer countMoviesByGenre(String genre);
 }
--- src/main/java/ua/kpi/coursework/service/MovieServiceImpl.java
+++ src/main/java/ua/kpi/coursework/service/MovieServiceImpl.java
@@ -1,6 +1,7 @@
 package ua.kpi.coursework.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import ua.kpi.coursework.domain.Movie;
 import ua.kpi.coursework.domain.MovieRepository;
@@ -17,7 +18,23 @@ public class MovieServiceImpl {
         this.movieRepository = movieRepository;
     }
 
-    public List<Movie> getByYear(Integer year) {
-        return movieRepository.findByYear(year);
+    public Integer getTotalByYear(Integer year) {
+        return movieRepository.countMoviesByYear(year);
+    }
+
+    public List<Movie> getMoviesByYear(Integer year, Pageable limit) {
+        return movieRepository.findMoviesByYear(year, limit);
+    }
+
+    public List<Movie> getMoviesByGenre(String genre, Pageable limit) {
+        return movieRepository.findMoviesByGenre(genre, limit);
+    }
+
+    public Integer getTotalByGenre(String genre) {
+        return movieRepository.countMoviesByGenre(genre);
+    }
+
+    public List<String> getUniqueGenres() {
+        return movieRepository.findDistinctGenre();
     }
 }
