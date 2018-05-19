72531a246398fe13716e27cbe6534f690301977b
Alexander Onbysh <onbysh.a@gmail.com>
2018-05-20 00:22:21 +0300

    Add movie functionality (#19)

--- src/main/java/ua/kpi/coursework/service/MovieServiceImpl.java
+++ src/main/java/ua/kpi/coursework/service/MovieServiceImpl.java
@@ -9,7 +9,7 @@ import ua.kpi.coursework.repository.MovieRepository;
 import java.util.List;
 
 @Service
-public class MovieServiceImpl {
+public class MovieServiceImpl implements MovieService {
 
     private final MovieRepository movieRepository;
 
@@ -22,19 +22,34 @@ public class MovieServiceImpl {
         return movieRepository.countMoviesByYear(year);
     }
 
+    @Override
     public List<Movie> getMoviesByYear(Integer year, Pageable limit) {
         return movieRepository.findMoviesByYear(year, limit);
     }
 
+    @Override
     public List<Movie> getMoviesByGenre(String genre, Pageable limit) {
         return movieRepository.findMoviesByGenre(genre, limit);
     }
 
+    @Override
     public Integer getTotalByGenre(String genre) {
         return movieRepository.countMoviesByGenre(genre);
     }
 
+    @Override
     public List<String> getUniqueGenres() {
         return movieRepository.findDistinctGenre();
+    }
+
+    @Override
+    public void save(Movie movie) {
+        movie.setDescription("Desc");
+        movie.setBudget("0");
+        movie.setRating((float) 0);
+        movie.setYear(2000);
+        movie.setDurability(120);
+
+        movieRepository.save(movie);
     }
 }
